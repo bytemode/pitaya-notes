43e5169c36356dc706160e44c027fecfb6c475c0
sunfeng45@163.com <sunfeng45@163.com>
2019-10-18 18:56:17 +0800

    添加goroutine上下文的一些封装

--- context/context.go
+++ context/context.go
@@ -27,9 +27,35 @@ import (
 	"github.com/topfreegames/pitaya/constants"
 )
 
+//上下文  多goroutine共享数据和多goroutine管理机制
+// type Context interface {
+//     Deadline() (deadline time.Time, ok bool)
+//     Done() <-chan struct{}
+//     Err() error
+//     Value(key interface{}) interface{}
+// }
+// Deadline 会返回一个超时时间，Goroutine获得了超时时间后，例如可以对某些io操作设定超时时间。
+// Done方   法返回一个信道（channel），当Context被撤销或过期时，该信道是关闭的，即它是一个表示Context是否已关闭的信号。
+// 当Done信道关闭后，Err方法表明Context被撤的原因。
+// Value可以让Goroutine共享一些数据，当然获得数据是协程安全的。但使用这些数据的时候要注意同步，比如返回了一个map，而这个map的读写则要加锁
+// Context结构也应该像一棵树，叶子节点须总是由根节点衍生出来的, 顶部的Goroutine应有办法主动关闭其下属的Goroutine的执行
+//context.Background返回根节点,返回空的context 它不能被取消、没有值、也没有过期时间func WithCancel(parent Context) (ctx Context, cancel CancelFunc)
+// 创建子孙节点
+// func WithCancel(parent Context) (ctx Context, cancel CancelFunc) 调用CancelFunc对象将撤销对应的Context对象
+// func WithDeadline(parent Context, deadline time.Time) (Context, CancelFunc) 过期时间由deadline和parent的过期时间共同决定
+// func WithTimeout(parent Context, timeout time.Duration) (Context, CancelFunc)  传入的是从现在开始Context剩余的生命时长
+// func WithValue(parent Context, key interface{}, val interface{}) Context 插入值使用Value(key)获取
+//子节点使用如下方法判断退出:
+// select {
+// case <-cxt.Done():
+// 	// do some clean...
+// }
+
+//向传播context中添加数据 key value
+
 // AddToPropagateCtx adds a key and value that will be propagated through RPC calls
 func AddToPropagateCtx(ctx context.Context, key string, val interface{}) context.Context {
-	propagate := ToMap(ctx)
+	propagate := ToMap(ctx) //取出ctx中的 constants.PropagateCtxKey所对应的map[string]interface{}{}
 	propagate[key] = val
 	return context.WithValue(ctx, constants.PropagateCtxKey, propagate)
 }
