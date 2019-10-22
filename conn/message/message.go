7e89e20e7fca2af03e0afbb4e9fa910b5e2f8a37
sunfeng45@163.com <sunfeng45@163.com>
2019-10-22 17:38:31 +0800

    message层注释

--- conn/message/message.go
+++ conn/message/message.go
@@ -31,19 +31,20 @@ type Type byte
 
 // Message types
 const (
-	Request  Type = 0x00 //客户端请求
-	Notify   Type = 0x01 //客户端通知
-	Response Type = 0x02 //服务器返回
-	Push     Type = 0x03 //服务器推送
+	Request  Type = 0x00 //客户端请求 ----000-
+	Notify   Type = 0x01 //客户端通知 ----001-
+	Response Type = 0x02 //服务器返回 ----010-
+	Push     Type = 0x03 //服务器推送 ----011-
 )
 
+//一些掩码定义用来操作二进制
 const (
 	errorMask            = 0x20
 	gzipMask             = 0x10
-	msgRouteCompressMask = 0x01
-	msgTypeMask          = 0x07
-	msgRouteLengthMask   = 0xFF
-	msgHeadLength        = 0x02
+	msgRouteCompressMask = 0x01 //启用路由压缩
+	msgTypeMask          = 0x07 //获取消息类型   00000111
+	msgRouteLengthMask   = 0xFF //消息路由的长度 11111111
+	msgHeadLength        = 0x02 //消息头的长度   00000010
 )
 
 var types = map[Type]string{
@@ -54,8 +55,8 @@ var types = map[Type]string{
 }
 
 var (
-	routes = make(map[string]uint16) // route map to code
-	codes  = make(map[uint16]string) // code map to route
+	routes = make(map[string]uint16) // route map to code  路由信息映射为证书
+	codes  = make(map[uint16]string) // code map to route  整数映射为路由信息
 )
 
 // Errors that could be occurred in message codec
@@ -86,18 +87,24 @@ var (
 // 现在消息共有四类，request，notify，response，push，值的范围是0～3。
 // 不同的消息类型有着不同的消息内容，下面会有详细分析。
 // 最后一位的route表示route是否压缩，影响route字段的长度。 这两部分之间相互独立，互不影响。
-// request   ----000  <message id> <route>
-// notify    ----001  <route>
-// response  ----010  <message id>
-// push      ----011  <route>
+// request   ----000-  <message id> <route>
+// notify    ----001-  <route>
+// response  ----010-  <message id>
+// push      ----011-  <route>
+
+// 路由压缩标志
+// 上图是不同的flag标志对应的route字段的内容：
+// flag的最后一位为1时，后面跟的是一个uInt16表示的route字典编号，需要通过查询字典来获取route;
+// flag最后一位为0是，后面route则由一个uInt8的byte，用来表示route的字节长度。
+// 之后是通过utf8编码后的route字 符串，其长度就是前面一位byte的uInt8的值，因此route的长度最大支持256B。
 
 // Message represents a unmarshaled message or a message which to be marshaled
 type Message struct {
-	Type       Type   // message type
-	ID         uint   // unique id, zero while notify mode
-	Route      string // route for locating service
-	Data       []byte // payload
-	compressed bool   // is message compressed
+	Type       Type   // message type 4中消息类型
+	ID         uint   // unique id, zero while notify mode 消息id（request response）
+	Route      string // route for locating service 消息路由
+	Data       []byte // payload  消息体的原始数据
+	compressed bool   // is message compressed 是否启用路由压缩
 	Err        bool   // is an error message
 }
 
@@ -130,6 +137,13 @@ func invalidType(t Type) bool {
 	return t < Request || t > Push
 
 }
+
+//启用路由压缩
+//对于服务端，server会扫描所有的Handler信息
+//对于客户端，用户需要配置一个路由映射表
+//通过这两种方式，pitaya会拿到所有的客户端和服务端的路由信息，然后将每一个路由信息都映射为一个小整数，
+//在客户端与服务器建立连接的握手过程中，服务器会将 整个字典传给客户端，
+//这样在以后的通信中，对于路由信息，将全部使用定义的小整数进行标记，大大地减少了额外信 息开销
 
 // SetDictionary set routes map which be used to compress route.
 func SetDictionary(dict map[string]uint16) error {
--- conn/message/message_encoder.go
+++ conn/message/message_encoder.go
@@ -34,7 +34,7 @@ type Encoder interface {
 
 // MessagesEncoder implements MessageEncoder interface
 type MessagesEncoder struct {
-	DataCompression bool
+	DataCompression bool //gzip压缩
 }
 
 // NewMessagesEncoder returns a new message encoder
@@ -47,6 +47,13 @@ func NewMessagesEncoder(dataCompression bool) *MessagesEncoder {
 func (me *MessagesEncoder) IsCompressionEnabled() bool {
 	return me.DataCompression
 }
+
+//message编码方式
+//消息头
+//----------flag(1byte) --------------------------------------------------->message id(0~5byte)------>route(0~256byte)
+//preserved(3bit) + 1bit(gzip) + message type(3bit) + route zip flag(1bit)   变长编码                 2byte的route code或1bite长度字符串
+//数据部分
+//根据是否启用gzip压缩在flag的第5个bit写标志,之后再消息头之后添加消息内容
 
 // Encode marshals message to binary format. Different message types is corresponding to
 // different message header, message types is identified by 2-4 bit of flag field. The
@@ -66,23 +73,25 @@ func (me *MessagesEncoder) Encode(message *Message) ([]byte, error) {
 		return nil, ErrWrongMessageType
 	}
 
+	//flag 1byte包含： 4bit保留 + 3bit(2 3 4)message type字段 + 1bit route标识
 	buf := make([]byte, 0)
-	flag := byte(message.Type) << 1
+	flag := byte(message.Type) << 1 //左移1bit 四中类型的type字段放在2 3 4bit
 
-	code, compressed := routes[message.Route]
+	code, compressed := routes[message.Route] //使用路由Route获取路由字典编码
 	if compressed {
-		flag |= msgRouteCompressMask
+		flag |= msgRouteCompressMask //0x01 写入路由压缩的标志 flag第一位写入路由压缩标识
 	}
 
 	if message.Err {
 		flag |= errorMask
 	}
 
-	buf = append(buf, flag)
+	buf = append(buf, flag) //标志位(flag)占用messagede第一个字节
 
+	//request和response需要携带message id
 	if message.Type == Request || message.Type == Response {
 		n := message.ID
-		// variant length encode
+		// variant length encode 可变长编码
 		for {
 			b := byte(n % 128)
 			n >>= 7
@@ -95,16 +104,20 @@ func (me *MessagesEncoder) Encode(message *Message) ([]byte, error) {
 		}
 	}
 
+	//route信息
 	if routable(message.Type) {
 		if compressed {
+			//如果启用路由压缩则使用2byte存储 route code
 			buf = append(buf, byte((code>>8)&0xFF))
 			buf = append(buf, byte(code&0xFF))
 		} else {
+			//如果不启用路由压缩则使用带1个字节长度的字串存储route 256B
 			buf = append(buf, byte(len(message.Route)))
 			buf = append(buf, []byte(message.Route)...)
 		}
 	}
 
+	//对消息体进行二进制压缩 gzip
 	if me.DataCompression {
 		d, err := compression.DeflateData(message.Data)
 		if err != nil {
@@ -112,12 +125,13 @@ func (me *MessagesEncoder) Encode(message *Message) ([]byte, error) {
 		}
 
 		if len(d) < len(message.Data) {
+			//压缩成功则将压缩后的值覆盖原有message.Data
 			message.Data = d
-			buf[0] |= gzipMask
+			buf[0] |= gzipMask //0x10  0001 0000  使用flag的第5位来存储gzip压缩标志
 		}
 	}
 
-	buf = append(buf, message.Data...)
+	buf = append(buf, message.Data...) //消息头和消息体合并在一起
 	return buf, nil
 }
 
@@ -133,14 +147,15 @@ func Decode(data []byte) (*Message, error) {
 		return nil, ErrInvalidMessage
 	}
 	m := New()
-	flag := data[0]
+	flag := data[0] //取出第一个字节作为flag
 	offset := 1
-	m.Type = Type((flag >> 1) & msgTypeMask)
+	m.Type = Type((flag >> 1) & msgTypeMask) //取出flag 2 3 4 bit作为消息的 type
 
 	if invalidType(m.Type) {
 		return nil, ErrWrongMessageType
 	}
 
+	//读取出message id base 128 variant
 	if m.Type == Request || m.Type == Response {
 		id := uint(0)
 		// little end byte order
@@ -161,6 +176,7 @@ func Decode(data []byte) (*Message, error) {
 
 	if routable(m.Type) {
 		if flag&msgRouteCompressMask == 1 {
+			//读取route code (2byte) 根据code得到route
 			m.compressed = true
 			code := binary.BigEndian.Uint16(data[offset:(offset + 2)])
 			route, ok := codes[code]
@@ -170,6 +186,7 @@ func Decode(data []byte) (*Message, error) {
 			m.Route = route
 			offset += 2
 		} else {
+			//读取一个字节作为字符串长度然后根据读取route字符串
 			m.compressed = false
 			rl := data[offset]
 			offset++
@@ -178,6 +195,7 @@ func Decode(data []byte) (*Message, error) {
 		}
 	}
 
+	//剩余部分为数据部分 根据flag的第5bit来进行解压
 	m.Data = data[offset:]
 	var err error
 	if flag&gzipMask == gzipMask {
